b72552a15ac15f49fa3a5a281f2d882f0658b221
simonq01 <simon.quirk@nhs.net>
2020-01-08 11:09:51 +0000

    Tidying up branch.

--- setup.R
+++
@@ -1,5 +0,0 @@
-#set up environment script for delayed discharges
-
-#Update with month, correct file paths etc. for each monthly publication
-
-#Use source("setup.R") in other syntax files
--- setup_environment.R
+++
@@ -1,3 +0,0 @@
-setwd("/conf/delayed_discharges/RAP development/R syntax/Jennifer")
-
-# add in file paths & dates for updating for each monthly publication.
